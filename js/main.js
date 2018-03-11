92d6a1b0b625ff8be26add3bdf2c565445262afa
labcode-de <leo@labcode.de>
2018-03-11 18:50:35 +0100

    Spenden hinzugefuegt und Footer angepasst

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
 $(function () {
-  $('.scrollspy').scrollSpy({
-    getActiveElement:
-      function (id) {
-        $('.navitem').removeClass('active');
-        $('#nav-' + id).addClass('active')
-      }
-  });
-  $('.modal').modal();
+  $('.scrollspy').scrollSpy();
 })
