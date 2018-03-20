04d0732a3cef52a3ea996df0f0c07ddc8c3c12d9
labcode-de <leo@labcode.de>
2018-03-20 21:18:03 +0100

    Links zu Kontaktformular

--- assets/js/kontakt.js
+++ assets/js/kontakt.js
@@ -1,3 +1,22 @@
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 $(function () {
-  $('select').material_select();
+  const param_rcp = getParameterByName('rcp')
+  if(param_rcp !== "") {
+    const rcp_option = $('#recipient>option[value=' + param_rcp + ']');
+    if(rcp_option.length === 1) {
+      rcp_option.attr('selected', true);
+      $('#recipient_option_choose').removeAttr('selected');
+      $('select').material_select();
+    }
+  } else {
+    $('select').material_select();
+  }
 })
