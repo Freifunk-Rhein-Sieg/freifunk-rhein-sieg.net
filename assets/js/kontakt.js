3b2878637d40b890e54de214e903a530dfaa65d9
Leo Maroni <leo@labcode.de>
2018-05-03 17:49:57 +0200

    Some style things and go to materialize 1.0.0-rc.1

--- assets/js/kontakt.js
+++ assets/js/kontakt.js
@@ -8,19 +8,18 @@ function getParameterByName(name, url) {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 $(function () {
-  const param_rcp = getParameterByName('rcp')
+  var param_rcp = getParameterByName('rcp');
   if(param_rcp !== "" && param_rcp !== null && param_rcp !== undefined) {
-    const rcp_option = $('#recipient>option[value=' + param_rcp + ']');
-    console.log(param_rcp)
+    var rcp_option = $('#recipient>option[value=' + param_rcp + ']');
     if(rcp_option.length === 1) {
-      console.log(rcp_option)
       rcp_option.attr('selected', true);
       $('#recipient_option_choose').removeAttr('selected');
-      $('select').material_select();
+        $('select').formSelect();
     } else {
-      $('select').material_select();
+      $('select').formSelect();
     }
   } else {
-    $('select').material_select();
+      $('select').formSelect();
+
   }
-})
+});
